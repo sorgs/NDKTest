bb2d8a43af0f5b0bf4ed99efa022a5fd50e4668b
sorgs <1042746391@qq.com>
2019-05-18 21:17:26 +0800

    feat(*):del

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,17 +6,9 @@
 
 #include "ProjectorFocusManagerExt.h"
 
-char *m_buf = NULL;
-
 extern "C"
-JNIEXPORT jbyteArray JNICALL
-Java_com_sorgs_ndktest_XgimiCamera_getCameraDate(JNIEnv *env, jobject instance, jint add) {
+JNIEXPORT void JNICALL
+Java_com_sorgs_ndktest_MainActivity_doSomething(JNIEnv *env, jobject instance) {
+    //do somethings
 
-    m_buf = (char *) mmap(0, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, add, 0);
-    if (m_buf == MAP_FAILED) {
-        perror("mmap: ");
-    }
-    jbyteArray array = env->NewByteArray(1024);
-    env->SetByteArrayRegion(array, 0, 1024, reinterpret_cast<const jbyte *>(m_buf));
-    return array;
 }
--- app/src/main/java/com/sorgs/ndktest/MainActivity.java
+++ app/src/main/java/com/sorgs/ndktest/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("native-lib");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        doSomething();
     }
+
+    public native final void doSomething();
 }
